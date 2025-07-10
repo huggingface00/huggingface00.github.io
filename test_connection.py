2ee2117f577fcd12be6276f366ee46e9946ddd4a
MohammadHR10 <mhr9808@mavs.uta.edu>
2025-07-09 20:55:00 -0500

    Updated website

--- test_connection.py
+++ test_connection.py
@@ -1,7 +1,19 @@
+# from neo4j import GraphDatabase
+
+# # URI examples: "neo4j://localhost", "neo4j+s://xxx.databases.neo4j.io"
+# URI = "neo4j+s://72da2fde.databases.neo4j.io"
+# AUTH = ("neo4j", "6OEkKBIcouM-Ngx6nIV1IXkDKsIBrKuvTIpVkB1730s")
+
+# with GraphDatabase.driver(URI, auth=AUTH) as driver:
+#     driver.verify_connectivity()
+
 from neo4j import GraphDatabase
 
-driver = GraphDatabase.driver("bolt://localhost:7687", auth=None)
+uri = "neo4j+s://72da2fde.databases.neo4j.io"
+auth = ("neo4j", "6OEkKBIcouM-Ngx6nIV1IXkDKsIBrKuvTIpVkB1730s")
+
+driver = GraphDatabase.driver(uri, auth=auth)
 
 with driver.session() as session:
-    result = session.run("RETURN 1 AS test")
-    print("✅ Connected successfully:", result.single()["test"])
+    result = session.run("RETURN 1")
+    print(result.single())
